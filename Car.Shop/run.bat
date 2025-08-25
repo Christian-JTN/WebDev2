1e6dcb557f4dcd61d82d8e33460a2ebfcb81b22c
Jekjek <christianjacob.nera@lorma.edu>
2025-08-25 12:59:29 +0800

    Add files via upload

---
+++ Car.Shop/run.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Starting Car Management System...
+echo.
+echo Make sure you have Java installed and in your PATH
+echo.
+echo If you have Maven installed, run: mvn spring-boot:run
+echo If you have the JAR file, run: java -jar target/car-0.0.1-SNAPSHOT.jar
+echo.
+echo The application will be available at: http://localhost:8080
+echo.
+pause
