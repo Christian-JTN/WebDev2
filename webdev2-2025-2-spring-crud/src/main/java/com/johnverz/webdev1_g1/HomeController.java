40b5c211f1141f0624b4cc7014241b4c116e3d65
Jekjek <christianjacob.nera@lorma.edu>
2025-08-29 09:25:12 +0800

    Add files via upload

---
+++ webdev2-2025-2-spring-crud/src/main/java/com/johnverz/webdev1_g1/HomeController.java
@@ -0,0 +1,63 @@
+package com.johnverz.webdev1_g1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    CarRepository carRepository;
+
+    public HomeController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<Car> cars = carRepository.findAll();
+        model.addAttribute("cars",carRepository.findAll());
+        cars.forEach(car -> {
+            System.out.println(car.getMake());
+        });
+        return "index";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        Car car = new Car();
+        model.addAttribute("car",car);
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Car car){
+        carRepository.save(car);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+        Car car = carRepository.findById(id).orElse(null);
+        model.addAttribute("car", car);
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Car car) {
+        carRepository.save(car);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        carRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
